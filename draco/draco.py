a9af3c34bf9a0b14cc7414d6da25fb6917c90760
KyleM73 <kyle.morgenstein@gmail.com>
2024-01-29 07:14:14 -0600

    draco

--- draco/draco.py
+++ draco/draco.py
@@ -12,19 +12,21 @@ from __future__ import annotations
 import os
 
 import omni.isaac.orbit.sim as sim_utils
-from omni.isaac.orbit.actuators import IdealPDActuatorCfg
+from omni.isaac.orbit.actuators import IdealPDActuatorCfg, ImplicitActuatorCfg
 from omni.isaac.orbit.assets.articulation import ArticulationCfg
 
 ##
 # Configuration
 ##
 
+ActuatorCfg = IdealPDActuatorCfg #ImplicitActuatorCfg
+
 DRACO_CFG = ArticulationCfg(
     spawn=sim_utils.UrdfFileCfg(
         fix_base=False,
         merge_fixed_joints=True,
         make_instanceable=True,
-        urdf_path=os.path.abspath(os.path.join(os.path.dirname(__file__), "draco.urdf")),
+        asset_path=os.path.abspath(os.path.join(os.path.dirname(__file__), "draco.urdf")),
         activate_contact_sensors=True,
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
@@ -36,21 +38,21 @@ DRACO_CFG = ArticulationCfg(
             max_depenetration_velocity=1.0,
         ),
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=True,
+            enabled_self_collisions=False,
             solver_position_iteration_count=4,
             solver_velocity_iteration_count=0,
         ),
     ),
     init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 1.0),
+        pos=(0.0, 0.0, 0.7),
         joint_pos={
             "[lr]_hip_ie.*" : 0.000,
             "l_hip_aa.*" : 0.160,
             "r_hip_aa.*" : -0.160,
-            "[lr]_hip_fe.*" : -0.785,
-            "[lr]_hip_fe_jp.*" : 0.785,
-            "[lr]_knee_fe_jd.*" : 0.785,
-            "[lr]_ankle_fe.*" : -0.785,
+            "[lr]_hip_fe.*" : -1.1, #-0.785,
+            "[lr]_knee_fe_jp.*" : 0.785,
+            "[lr]_knee_fe_jd.*" : 1.1, #0.785,
+            "[lr]_ankle_fe.*" : -1.1, #-0.785,
             "l_ankle_ie.*" : -0.160,
             "r_ankle_ie.*" : 0.160,
             "[lr]_shoulder_fe.*" : 0.000,
@@ -64,8 +66,9 @@ DRACO_CFG = ArticulationCfg(
         },
         joint_vel={".*": 0.000},
     ),
+    soft_joint_pos_limit_factor=0.9,
     actuators={
-        "pitch": IdealPDActuatorCfg(
+        "pitch": ActuatorCfg(
             joint_names_expr=["neck_pitch.*", "[lr]_wrist_pitch.*"],
             effort_limit=8.80,
             velocity_limit=76.0,
@@ -82,7 +85,7 @@ DRACO_CFG = ArticulationCfg(
                 "[lr]_wrist_pitch.*": 0.0
             },
         ),
-         "shoulder": IdealPDActuatorCfg(
+         "shoulder": ActuatorCfg(
             joint_names_expr=["[lr]_shoulder.*"],
             effort_limit=18.0,
             velocity_limit=47.0,
@@ -100,7 +103,7 @@ DRACO_CFG = ArticulationCfg(
                 "[lr]_shoulder.*": 0.0,
             },
          ),
-        "elbow": IdealPDActuatorCfg(
+        "elbow": ActuatorCfg(
             joint_names_expr=["[lr]_elbow_fe.*"],
             effort_limit=10.0,
             velocity_limit=59.2,
@@ -114,7 +117,7 @@ DRACO_CFG = ArticulationCfg(
                 "[lr]_elbow_fe.*": 0.0,
             },
         ),
-        "wrist": IdealPDActuatorCfg(
+        "wrist": ActuatorCfg(
             joint_names_expr=["[lr]_wrist_ps.*"],
             effort_limit=12.0,
             velocity_limit=71.0,
@@ -128,24 +131,24 @@ DRACO_CFG = ArticulationCfg(
                 "[lr]_wrist_ps.*": 0.0,
             },
         ),
-        "hip_ie_ankle_fe": IdealPDActuatorCfg(
+        "hip_ie_ankle_fe": ActuatorCfg(
             joint_names_expr=["[lr]_hip_ie.*", "[lr]_ankle_fe.*"],
             effort_limit=44.0,
             velocity_limit=8.1,
             stiffness={
                 "[lr]_hip_ie.*": 150.0,
-                "[lr]_ankle_fe.*": 150.0,
+                "[lr]_ankle_fe.*": 200.0, #150.0,
             },
             damping={
                 "[lr]_hip_ie.*": 1.0,
-                "[lr]_ankle_fe.*": 0.2,
+                "[lr]_ankle_fe.*": 2.0, #0.2,
             },
             friction={
                 "[lr]_hip_ie.*": 0.0,
                 "[lr]_ankle_fe.*": 0.0,
             },
         ),
-        "hip_aa": IdealPDActuatorCfg(
+        "hip_aa": ActuatorCfg(
             joint_names_expr=["[lr]_hip_aa.*"],
             effort_limit=56.0,
             velocity_limit=5.1,
@@ -159,7 +162,7 @@ DRACO_CFG = ArticulationCfg(
                 "[lr]_hip_aa.*": 0.0,
             },
         ),
-        "hip_fe": IdealPDActuatorCfg(
+        "hip_fe": ActuatorCfg(
             joint_names_expr=["[lr]_hip_fe.*"],
             effort_limit=59.6,
             velocity_limit=36.7,
@@ -173,21 +176,21 @@ DRACO_CFG = ArticulationCfg(
                 "[lr]_hip_fe.*": 0.0,
             },
         ),
-        "knee_fe": IdealPDActuatorCfg(
+        "knee_fe": ActuatorCfg(
             joint_names_expr=["[lr]_knee_fe.*"],
             effort_limit=40.85,
             velocity_limit=13.35,
             stiffness={
-                "[lr]_knee_fe.*": 350.0, # right: 450 difference from hardware
+                "[lr]_knee_fe.*": 450.0, #450.0, # right: 450 difference from hardware
             },
             damping={
-                "[lr]_knee_fe.*": 1.5,
+                "[lr]_knee_fe.*": 5.0, #1.5,
             },
             friction={
                 "[lr]_knee_fe.*": 0.0,
             },
         ),
-        "ankle_ie": IdealPDActuatorCfg(
+        "ankle_ie": ActuatorCfg(
             joint_names_expr=["[lr]_ankle_ie.*"],
             effort_limit=30.0,
             velocity_limit=11.1,
@@ -203,3 +206,33 @@ DRACO_CFG = ArticulationCfg(
         ),
     }
 )
+"""
+Ordered Joints:
+0:  'l_hip_ie',
+1:  'l_shoulder_fe',
+2:  'neck_pitch',
+3:  'r_hip_ie',
+4:  'r_shoulder_fe',
+5:  'l_hip_aa',
+6:  'l_shoulder_aa',
+7:  'r_hip_aa',
+8:  'r_shoulder_aa',
+9:  'l_hip_fe',
+10: 'l_shoulder_ie',
+11: 'r_hip_fe',
+12: 'r_shoulder_ie',
+13: 'l_knee_fe_jp',
+14: 'l_elbow_fe',
+15: 'r_knee_fe_jp',
+16: 'r_elbow_fe',
+17: 'l_knee_fe_jd',
+18: 'l_wrist_ps',
+19: 'r_knee_fe_jd',
+20: 'r_wrist_ps',
+21: 'l_ankle_fe',
+22: 'l_wrist_pitch',
+23: 'r_ankle_fe',
+24: 'r_wrist_pitch',
+25: 'l_ankle_ie',
+26: 'r_ankle_ie']
+"""
